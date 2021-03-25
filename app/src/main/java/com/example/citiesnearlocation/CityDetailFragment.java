bba5ed88b05a22cecf9b58c183ab1d31394901be
KyleChutjian <kyle.chutjian@gmail.com>
2021-03-25 14:15:44 -0400

    Created new empty fragment

---
+++ app/src/main/java/com/example/citiesnearlocation/CityDetailFragment.java
@@ -0,0 +1,14 @@
+package com.example.citiesnearlocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CityDetailFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_detail_fragment);
+    }
+}
